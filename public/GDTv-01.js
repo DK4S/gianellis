7db95ec0203538cb2ab11af4084746517089bef9
DK4S <93524552+DK4S@users.noreply.github.com>
2024-02-05 10:14:16 -0500

    f

--- public/GDTv-01.js
+++ public/GDTv-01.js
@@ -2,14 +2,22 @@
 
 //////////////////////// Section 1 User Interface  ////////////////////////
 
-//Navbar functions.
+//Navbar
 
 function openNav() {
   document.getElementById("hamburgerStack").style.display = "block";
-  document.getElementById("paypal-button-container").style.display = "none";
 
 }
 function closeNav() {
   document.getElementById("hamburgerStack").style.display = "";
-  document.getElementById("paypal-button-container").style.display = "block";
+}
+
+//Order Cluster
+
+function openOrderStack() {
+  document.getElementById("order-overlay").style.display = "block";
+
+}
+function closeOrderStack() {
+  document.getElementById("order-overlay").style.display = "";
 }
